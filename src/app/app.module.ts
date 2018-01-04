4263dffa789f0b9f42fa3130a2c450c5d806bf48
AcarMeel <acarmeel@gmail.com>
2018-01-03 21:45:55 -0600

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,7 @@ import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graph1Component } from './pages/graph1/graph1.component';
+
 
 
 
